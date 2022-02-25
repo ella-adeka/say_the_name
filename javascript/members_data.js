d71529a2f9e61d59f60a9bc2b671fedb42f87a83
Emmanuella Adeka <onyiadeka@gmail.com>
2022-02-25 23:10:58 +0100

    created array to store members data

---
+++ javascript/members_data.js
@@ -0,0 +1,82 @@
+const membersData = [
+    {
+        id: "01",
+        name: 'S.coups',
+        image: 'jun',
+        team: 'Hip-Hop Team'
+    },
+    {
+        id: "02",
+        name: 'Jeonghan',
+        image: 'jun_2',
+        team: 'Vocal Team'
+    },
+    {
+        id: "03",
+        name: 'Joshua',
+        image: 'josh',
+        team: 'Vocal Team'
+    },
+    {
+        id: "04",
+        name: 'Jun',
+        image: 'jun_2',
+        team: 'Performance Team'
+    },
+    {
+        id: "05",
+        name: 'Hoshi',
+        image: 'jun_2',
+        team: 'Performance Team'
+    },
+    {
+        id: "06",
+        name: 'Wonwoo',
+        image: 'jun_2',
+        team: 'Hip-Hop Team'
+    },
+    {
+        id: "07",
+        name: 'Woozi',
+        image: 'jun_2',
+        team: 'Vocal Team'
+    },
+    {
+        id: "08",
+        name: 'The8',
+        image: 'jun_2',
+        team: 'Performance Team'
+    },
+    {
+        id: "09",
+        name: 'Mingyu',
+        image: 'jun_2',
+        team: 'Hip-Hop Team'
+    },
+    {
+        id: "10",
+        name: 'DK',
+        image: 'jun_2',
+        team: 'Vocal Team'
+    },
+    {
+        id: "11",
+        name: 'Seungwan',
+        image: 'jun_2',
+        team: 'Vocal Team'
+    },
+    {
+        id: "12",
+        name: 'Vernon',
+        image: 'jun_2',
+        team: 'Hip-Hop Team'
+    },
+    {
+        id: "13",
+        name: 'Dino',
+        image: 'jun_2',
+        team: 'Performance Team'
+    },
+]
+
+// export default membersData;
