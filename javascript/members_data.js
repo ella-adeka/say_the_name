9ba62781e0aae1134951fb4901e0511d3d44d26b
Emmanuella Adeka <onyiadeka@gmail.com>
2022-02-26 23:38:16 +0100

    updated images for each member

--- javascript/members_data.js
+++ javascript/members_data.js
@@ -2,79 +2,79 @@ const membersData = [
     {
         id: "01",
         name: 'S.coups',
-        image: 'jun',
+        image: 'scoups_attacca_op1',
         team: 'Hip-Hop Team'
     },
     {
         id: "02",
         name: 'Jeonghan',
-        image: 'jun_2',
+        image: 'jeonghan_attacca_op1',
         team: 'Vocal Team'
     },
     {
         id: "03",
         name: 'Joshua',
-        image: 'josh',
+        image: 'joshua_attacca_op1',
         team: 'Vocal Team'
     },
     {
         id: "04",
         name: 'Jun',
-        image: 'jun_2',
+        image: 'jun_attacca_op1',
         team: 'Performance Team'
     },
     {
         id: "05",
         name: 'Hoshi',
-        image: 'jun_2',
+        image: 'hoshi_attacca_op1',
         team: 'Performance Team'
     },
     {
         id: "06",
         name: 'Wonwoo',
-        image: 'jun_2',
+        image: 'wonwoo_attacca_op1',
         team: 'Hip-Hop Team'
     },
     {
         id: "07",
         name: 'Woozi',
-        image: 'jun_2',
+        image: 'woozi_attacca_op1',
         team: 'Vocal Team'
     },
     {
         id: "08",
         name: 'The8',
-        image: 'jun_2',
+        image: 'minghao_attacca_op1',
         team: 'Performance Team'
     },
     {
         id: "09",
         name: 'Mingyu',
-        image: 'jun_2',
+        image: 'mingyu_attacca_op1',
         team: 'Hip-Hop Team'
     },
     {
         id: "10",
         name: 'DK',
-        image: 'jun_2',
+        image: 'dk_attacca_op1',
         team: 'Vocal Team'
     },
     {
         id: "11",
-        name: 'Seungwan',
-        image: 'jun_2',
+        name: 'Seungkwan',
+        image: 'seungkwan_attacca_op1',
         team: 'Vocal Team'
     },
     {
         id: "12",
         name: 'Vernon',
-        image: 'jun_2',
+        image: 'vernon_attacca_op1',
         team: 'Hip-Hop Team'
     },
     {
         id: "13",
         name: 'Dino',
-        image: 'jun_2',
+        image: 'dino_attacca_op1',
         team: 'Performance Team'
     },
 ]
