1deaa4bda4f7869bfa5c9c35f6d7ba1c28fa2c9d
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-03-21 20:07:45 +0100

    first commit

---
+++ javascript/data/members_data.js
@@ -0,0 +1,147 @@
+const membersData = [
+    {
+        id: "01",
+        stage_name: 'S.coups',
+        korean_stage_name: '(에스쿱스)',
+        birth_name: 'Choi, Seung Cheol (최승철)',
+        image: 'scoups_attacca_op1',
+        sub_unit: 'Hip-Hop Team, SVT Leaders',
+        position: 'Leader, Hip-Hop Team Leader, Rapper, Sub Vocalist',
+        birthday: 'August 8, 1995',
+        zodiac_sign: 'Leo'
+    },
+    {
+        id: "02",
+        stage_name: 'Jeonghan',
+        korean_stage_name: '(정한)',
+        birth_name: 'Yoon, Jeong Han (윤정한)',
+        image: 'jeonghan_attacca_op1',
+        sub_unit: 'Vocal Team',
+        position: 'Lead Vocalist, Visual',
+        birthday: 'October 4, 1995',
+        zodiac_sign: 'Libra'
+    },
+    {
+        id: "03",
+        stage_name: 'Joshua',
+        korean_stage_name: '(조슈아)',
+        birth_name: 'Hong, Ji Soo (홍지수)',
+        image: 'joshua_attacca_op1',
+        sub_unit: 'Vocal Team',
+        position: 'Lead Vocalist, Visual',
+        birthday: 'December 30, 1995',
+        zodiac_sign: 'Capricorn'
+    },
+    {
+        id: "04",
+        stage_name: 'Jun',
+        korean_stage_name: '(준)',
+        birth_name: 'Moon, Jun Hwi (문준휘)',
+        image: 'jun_attacca_op1',
+        sub_unit: 'Performance Team',
+        position: 'Lead Dancer, Sub Vocalist',
+        birthday: 'June 10, 1996',
+        zodiac_sign: 'Gemini'
+    },
+    {
+        id: "05",
+        stage_name: 'Hoshi',
+        korean_stage_name: '(호시)',
+        birth_name: 'Kwon, Soon Young (권순영)',
+        image: 'hoshi_attacca_op1',
+        sub_unit: 'Performance Team, SVT Leaders, BooSeokSoon',
+        position: 'Performance Team Leader, Main Dancer, Lead Vocalist, Sub Rapper',
+        birthday: 'June 15, 1996',
+        zodiac_sign: 'Gemini'
+    },
+    {
+        id: "06",
+        stage_name: 'Wonwoo',
+        korean_stage_name: '(원우)',
+        birth_name: 'Jeon, Won Woo (전원우)',
+        image: 'wonwoo_attacca_op1',
+        sub_unit: 'Hip-Hop Team',
+        position: 'Rapper, Sub Vocalist',
+        birthday: 'July 17, 1996',
+        zodiac_sign: 'Cancer'
+    },
+    {
+        id: "07",
+        stage_name: 'Woozi',
+        korean_stage_name: '(우지)',
+        birth_name: 'Lee, Ji Hoon (이지훈)',
+        image: 'woozi_attacca_op1',
+        sub_unit: 'Vocal Team, SVT Leaders',
+        position: 'Vocal Team Leader, Lead Vocalist, Producer',
+        birthday: 'November 22, 1996',
+        zodiac_sign: 'Scorpio/Sagittarius cusp'
+    },
+    {
+        id: "08",
+        stage_name: 'The8',
+        korean_stage_name: '(디에잇)',
+        birth_name: 'Xu, Ming Hao (徐明浩)',
+        image: 'minghao_attacca_op1',
+        sub_unit: 'Performance Team',
+        position: 'Vocal Team Leader, Lead Vocalist, Producer',
+        birthday: 'November 7, 1997',
+        zodiac_sign: 'Scorpio'
+    },
+    {
+        id: "09",
+        stage_name: 'Mingyu',
+        korean_stage_name: '(민규)',
+        birth_name: 'Kim, Min Gyu (김민규)',
+        image: 'mingyu_attacca_op1',
+        sub_unit: 'Hip-Hop Team',
+        position: 'Rapper, Sub Vocalist, Visual, Face of the Group',
+        birthday: 'April 6, 1997',
+        zodiac_sign: 'Aries'
+    },
+    {
+        id: "10",
+        stage_name: 'DK',
+        korean_stage_name: '(도겸)',
+        birth_name: 'Lee, Seok Min (이석민)',
+        image: 'dk_attacca_op1',
+        sub_unit: 'Vocal Team, BooSeokSoon',
+        position: 'Main Vocalist',
+        birthday: 'February 18, 1997',
+        zodiac_sign: 'Aquarius/Pisces Cusp'
+    },
+    {
+        id: "11",
+        stage_name: 'Seungkwan',
+        korean_stage_name: '(승관)',
+        birth_name: 'Boo, Seung Kwan (부승관)',
+        image: 'seungkwan_attacca_op1',
+        sub_unit: 'Vocal Team, BooSeokSoon',
+        position: 'Main Vocalist, Face of the Group',
+        birthday: 'January 16, 1998',
+        zodiac_sign: 'Capricorn'
+    },
+    {
+        id: "12",
+        stage_name: 'Vernon',
+        korean_stage_name: '(버논)',
+        birth_name: 'Hansol, Vernon Chwe',
+        image: 'vernon_attacca_op1',
+        sub_unit: 'Hip-Hop Team',
+        position: 'Rapper, Sub Vocalist, Visual, Face of the Group',
+        birthday: 'February 18, 1998',
+        zodiac_sign: 'Aquarius/Pisces Cusp'
+    },
+    {
+        id: "13",
+        stage_name: 'Dino',
+        korean_stage_name: '(디노)',
+        birth_name: 'Lee, Chan (이찬)',
+        image: 'dino_attacca_op1',
+        sub_unit: 'Performance Team',
+        position: 'Main Dancer, Sub Vocalist, Sub Rapper, Maknae',
+        birthday: 'February 11, 1999',
+        zodiac_sign: 'Aquarius'
+    },
+]
+
+// export default membersData;
