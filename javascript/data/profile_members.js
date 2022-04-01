d938093c4c0b4a79ab190db3f554b31be3e1fc9a
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-04-01 22:59:43 +0100

    no changes

---
+++ javascript/data/profile_members.js
@@ -0,0 +1,69 @@
+// use op3
+
+const profileMembersData = [
+    {
+        id: "01",
+        name: "&#183; S.Coups",
+        image: "scoups_attacca_op1"
+    },
+    {
+        id: "02",
+        name: "Jeonghan",
+        image: "jeonghan_attacca_op1"
+    },
+    {
+        id: "03",
+        name: "Joshua",
+        image: "joshua_attacca_op1"
+    },
+    {
+        id: "04",
+        name: "Jun",
+        image: "jun_attacca_op1"
+    },
+    {
+        id: "05",
+        name: "Hoshi",
+        image: "hoshi_attacca_op1"
+    },
+    {
+        id: "06",
+        name: "Wonwoo",
+        image: "wonwoo_attacca_op1"
+    },
+    {
+        id: "07",
+        name: "Woozi",
+        image: "woozi_attacca_op1"
+    },
+    {
+        id: "08",
+        name: "Dokyeom",
+        image: "dk_attacca_op1"
+    },
+    {
+        id: "09",
+        name: "Mingyu",
+        image: "mingyu_attacca_op1"
+    },
+    {
+        id: "10",
+        name: "Minghao",
+        image: "minghao_attacca_op1"
+    },
+    {
+        id: "11",
+        name: "Seungkwan",
+        image: "seungkwan_attacca_op1"
+    },
+    {
+        id: "12",
+        name: "Vernon",
+        image: "vernon_attacca_op1"
+    },
+    {
+        id: "13",
+        name: "Dino",
+        image: "dino_attacca_op1"
+    },
+]
