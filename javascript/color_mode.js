b170f0c37c631404f72a18b7281516ee62463491
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-04-01 23:00:45 +0100

    added code to display fandom color as background images but commented it

--- javascript/color_mode.js
+++ javascript/color_mode.js
@@ -1,27 +1,40 @@
 
 let colorModeToggle = false;
 const body = document.querySelector("body");
-const link = document.querySelector(".link");
 const cursor = document.querySelector("#cursor");
 const navBar = document.querySelector(".navbar");
+const membersInfo = document.querySelectorAll(".mainMembers__dets__info");
+
 const colorMode = document.querySelector("#colorMode");
+
 
 colorMode.addEventListener("click", toggleMode);
 
 function toggleMode() {
     
-    if (!colorModeToggle && !body.classList.contains('dark') && !link.classList.contains('dark') && !cursor.classList.contains('dark') ) {
+    if (!colorModeToggle && !body.classList.contains('dark')   && !cursor.classList.contains('dark') ) {
         body.classList.add('dark');
-        link.classList.add('dark');
+        membersInfo.forEach(el => { el.classList.add('dark')});
         cursor.classList.add('dark');
         navBar.classList.add('dark');
         colorMode.style.borderColor = "white";
         body.style.transition = "2s ease-in";
         localStorage.setItem('mode', 'dark');
         colorModeToggle = true;
-    } else {
+    }
+    // } else if (!colorModeToggle && !body.classList.contains('dark') && !body.classList.contains('fandomColor') ){
+    //     body.classList.add('fandomColor');
+    //     // membersInfo.forEach(el => { el.classList.add('dark')});
+    //     // cursor.classList.add('dark');
+    //     // navBar.classList.add('dark');
+    //     colorMode.style.borderColor = "#92a8d1";
+    //     body.style.transition = "2s ease-in";
+    //     localStorage.setItem('mode', 'fandomColor');
+    //     colorModeToggle = true;
+    // } 
+    else {
         body.classList.remove('dark');
-        link.classList.remove('dark');
+        membersInfo.forEach(el => { el.classList.remove('dark')});
         cursor.classList.remove('dark');
         navBar.classList.remove('dark');
         colorMode.style.borderColor = "black";
@@ -33,9 +46,13 @@ function toggleMode() {
 
 if (localStorage.getItem("mode") === "dark") {
     body.classList.add('dark');
-    link.classList.add('dark');
+    membersInfo.forEach(el => { el.classList.add('dark')});
     cursor.classList.add('dark');
     navBar.classList.add('dark');
     colorMode.style.borderColor = "white";
-}
+} 
+
+// if (localStorage.getItem("mode") === "fandomColor"){
+//     body.classList.add('fandomColor');
+// } 
 
