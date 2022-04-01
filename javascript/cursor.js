5c096927aa11f0dbde9af5410e68f9577545ee05
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-04-01 23:01:20 +0100

    added more events for cursor

--- javascript/cursor.js
+++ javascript/cursor.js
@@ -6,6 +6,17 @@ document.addEventListener("mousemove", (e) => {
     cursor.setAttribute("style","top: "+(e.clientY - scrollY)+"px; left: "+(e.clientX)+"px");
 });
 
+document.addEventListener("load", () => {
+    cursor.setAttribute("style", "display:none;");
+});
+document.addEventListener("mouseout", () => {
+    cursor.setAttribute("style", "display:none;");
+});
+
+document.addEventListener("mouseenter", () => {
+    cursor.setAttribute("style", "display:inline;");
+});
+
 document.addEventListener("click", () => {
     cursor.classList.add("expand");
 
