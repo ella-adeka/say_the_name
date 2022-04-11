b66c1c9db58e9a81b7dcb4a5665c6d134994921a
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-04-11 23:54:09 +0100

    added code to save data to localStorage

--- javascript/pages/discography.js
+++ javascript/pages/discography.js
@@ -13,63 +13,95 @@ const main = document.querySelector('.mainDiscography');
     
     // Initialize and assign value to i;
     let i = 0;
-
+    // localStorage.setItem("album", `${discographyData[i].album_name}`);
     function prevAlbum() {
         if (i !== 0) {
             i--;
+            localStorage.setItem("album", `${discographyData[i].album_name}`);
         } 
     }
 
     function nextAlbum() {
         if (i < discographyData.length) {
             i++;
+            localStorage.setItem("album", `${discographyData[i].album_name}`);
         } 
     }
 
     prev.addEventListener("click", () => {
         prevAlbum();
         showAlbums();
+        
     });
 
     next.addEventListener("click", () => {
         nextAlbum();
-        showAlbums();
+        if (localStorage.getItem("album") === `${discographyData[i].album_name}`) {
+            showAlbums();
+        }
+        
+        
     });
     
+    // if (localStorage.getItem("album") === `${discographyData[i].album_name}`) {
+    //     showAlbums();
+    // }
+
+    // function showAlbumsInStorage() {
+    //     showAlbums();
+    //     // localStorage.setItem("album", `${discographyData[i].album_name}`);
+    //     localStorage.setItem("album",`${discographyData[i].album_name}`);
+           
+    // }
+    // localStorage.getItem("album") === `${discographyData[i].album_name}`
+    
+    // if (localStorage.getItem("album", `${discographyData[i].album_name}`)) {   
         
     function showAlbums() {
-        if (i<discographyData.length) {
-            let songList = discographyData[i].tracks.length;
-            let track = "<ol class='mainDiscography__discography__dets__details__paragraph__track_list__ol'>";
-            for (let j = 0; j < songList; j++) {
-                track  += "<li class='mainDiscography__discography__dets__details__paragraph__track_list__ol__li'>" + discographyData[i].tracks[j] + "</li>";
+        // localStorage.setItem("album", `${discographyData[i].album_name}`);
+        // 
+            // showAlbums();
+        // }
+        // showAlbumsInStorage()
+        // if (i<discographyData.length) {
+            divUnderMain.innerHTML = localStorage.getItem("album"); 
+            // localStorage.getItem("album") == `${discographyData[i].album_name}`;
+            if(localStorage.getItem("album") === `${discographyData[i].album_name}`){
+                let songList = discographyData[i].tracks.length;
+                let track = "<ol class='mainDiscography__discography__dets__details__paragraph__track_list__ol'>";
+                for (let j = 0; j < songList; j++) {
+                    track  += "<li class='mainDiscography__discography__dets__details__paragraph__track_list__ol__li'>" + discographyData[i].tracks[j] + "</li>";
+                }
+                track+= "</ol>";
+
+                listenLink.setAttribute('href',`${discographyData[i].album_link}`);
+                watchLink.setAttribute('href',`${discographyData[i].youtube_link}`);
+
+                dets.innerHTML = `
+                <p class="mainDiscography__discography__dets__album_category">${discographyData[i].category}</p>
+                    <p class="mainDiscography__discography__dets__album_name">${discographyData[i].album_name}</p>
+                    <p class="mainDiscography__discography__dets__title_track">${discographyData[i].title_track}</p>
+                    <div class="mainDiscography__discography__dets__details">
+                        <div class="mainDiscography__discography__dets__details__imageDiv">
+                            <img class="mainDiscography__discography__dets__details__imageDiv__image" src=./images/discography/${discographyData[i].image} />
+                            <br>
+                            <br>
+                            <br>
+                        </div>
+                        <div class="mainDiscography___discography_dets__details__paragraph">
+                            <p class="mainDiscography__discography__dets__details__paragraph__album_detail">${discographyData[i].album_detail}</p>
+                            <p class="mainDiscography__discography__dets__details__paragraph__artist_name">${discographyData[i].artist_name}</p>
+                            <p class="mainDiscography__discography__dets__details__paragraph__release_date">${discographyData[i].release_date}</p>
+                            <p class="mainDiscography__discography__dets__details__paragraph__track_list">${track}</p>
+                        </div>
+                    </div>
+                    <p class="mainDiscography__discography__dets__id">${discographyData[i].id}/${discographyData.length}</p>
+                `;
+            } else{
+                console.log("no?"); 
+                // localStorage.setItem("album", `${discographyData[i].album_name}`);
             }
-            track+= "</ol>";
-
-            listenLink.setAttribute('href',`${discographyData[i].album_link}`);
-            watchLink.setAttribute('href',`${discographyData[i].youtube_link}`);
-
-            dets.innerHTML = `
-            <p class="mainDiscography__discography__dets__album_category">${discographyData[i].category}</p>
-                <p class="mainDiscography__discography__dets__album_name">${discographyData[i].album_name}</p>
-                <p class="mainDiscography__discography__dets__title_track">${discographyData[i].title_track}</p>
-                <div class="mainDiscography__discography__dets__details">
-                    <div class="mainDiscography__discography__dets__details__imageDiv">
-                        <img class="mainDiscography__discography__dets__details__imageDiv__image" src=./images/discography/${discographyData[i].image} />
-                        <br>
-                        <br>
-                        <br>
-                    </div>
-                    <div class="mainDiscography___discography_dets__details__paragraph">
-                        <p class="mainDiscography__discography__dets__details__paragraph__album_detail">${discographyData[i].album_detail}</p>
-                        <p class="mainDiscography__discography__dets__details__paragraph__artist_name">${discographyData[i].artist_name}</p>
-                        <p class="mainDiscography__discography__dets__details__paragraph__release_date">${discographyData[i].release_date}</p>
-                        <p class="mainDiscography__discography__dets__details__paragraph__track_list">${track}</p>
-                    </div>
-                </div>
-                <p class="mainDiscography__discography__dets__id">${discographyData[i].id}/${discographyData.length}</p>
-            `;
-        }
+        // }
         
         // IF NO PREVIOUS ALBUM
         if (i == 0){
@@ -85,5 +117,13 @@ const main = document.querySelector('.mainDiscography');
             next.style.display = "block";
         }
     }
+// }
+
+// if(localStorage.getItem("album") != `${discographyData[i].album_name}`){
+//     localStorage.setItem("album", `${discographyData[i].album_name}`);
+// } else{
+//     console.log("no? 2"); 
+// }
+
     
 divUnderMain.append(dets);
