f99427d1336cbb0db02fe90734d6080fbed370bf
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-04-28 20:19:44 +0100

    moved everyhting concerning the navbar from app.js

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,12 +1,64 @@
-const homeLink = document.querySelector(".home_link");
-const profileLink = document.querySelector(".profile_link");
+const menuBtn = document.querySelector('.menu_btn');
+const hamburger = document.querySelector('.menu_btn__burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.navbar');
+const navItem = document.querySelector('.navbar__item');
 
-homeLink.addEventListener("mouseover", () => {
-    // homeLink.style.color = "pink";
-    homeLink.style.cssText = "background: url('/images/members/woozi_attacca_op1.jpg') no-repeat fixed center;background-size: 600px; color:pink";
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if(!showMenu) {
+    hamburger.classList.add('open');
+    nav.classList.add('open');
+    menuNav.classList.add('open');
+    navItem.classList.add('open');
+
+    showMenu = true;
+  } else {
+    hamburger.classList.remove('open');
+    nav.classList.remove('open');
+    menuNav.classList.remove('open');
+    navItem.classList.remove('open');
+
+    showMenu = false;
+  }
+}
+
+// NAVBAR ANIMATION
+const homeLink = document.querySelector(".homeLink");
+const profileLink = document.querySelector(".profileLink");
+const discographyLink = document.querySelector(".discographyLink");
+const membersLink = document.querySelector(".membersLink");
+var tl = gsap.timeline();
+
+homeLink.addEventListener("mouseover", () =>{
+    tl.to(".home-project-preview", {duration: 2, width: "50%",ease: "Expo.easeInOut"});
+});
+homeLink.addEventListener("mouseout", () =>{
+    tl.to(".home-project-preview", {duration: .2, width: "0%",ease: "Expo.easeInOut"});
 });
 
-profileLink.addEventListener("mouseover", () => {
-    // profileLink.style.color = "pink";
-    profileLink.style.cssText = "background: url('/images/members/wonwoo_attacca_op1.jpg') no-repeat fixed center;background-size: cover; color:pink";
+profileLink.addEventListener("mouseover", () =>{
+    tl.to(".profile-project-preview", {duration: 1.2, width: "100%",ease: "Expo.easeInOut"});
 });
+profileLink.addEventListener("mouseout", () =>{
+    tl.to(".profile-project-preview", {duration: .2, width: "0%",ease: "Expo.easeInOut"});
+});
+
+discographyLink.addEventListener("mouseover", () =>{
+    tl.to(".discography-project-preview", {duration: 1.2, width: "55%",ease: "Expo.easeInOut"});
+});
+discographyLink.addEventListener("mouseout", () =>{
+    tl.to(".discography-project-preview", {duration: .2, width: "0%",ease: "Expo.easeInOut"});
+});
+
+membersLink.addEventListener("mouseover", () =>{
+    tl.to(".members-project-preview", {duration: 1.2, width: "60%",ease: "Expo.easeInOut"});
+});
+membersLink.addEventListener("mouseout", () =>{
+    tl.to(".members-project-preview", {duration: .2, width: "0%",ease: "Expo.easeInOut"});
+});
+// END OF NAVBAR ANIMATION
