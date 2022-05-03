5cec6f50faa02133771ac484d2cba56659aa4cec
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-05-03 23:33:00 +0100

    new file for each Members Image

---
+++ javascript/data/eachMembersData.js
@@ -0,0 +1,20 @@
+const eachMembersData = [
+    {
+        id: "01",
+        stage_name: 'S.coups',
+        image1: 'scoups_attacca_op1',
+        image2: 'wonwoo_attacca_op1',
+        image3: 'scoups_attacca_op1',
+        image4: 'scoups_attacca_op1',
+        image5: 'scoups_attacca_op1',
+    },
+    {
+        id: "02",
+        stage_name: 'Jeonghan',
+        image1: 'jeonghan_attacca_op1',
+        image2: 'jeonghan_attacca_op1',
+        image3: 'jeonghan_attacca_op1',
+        image4: 'jeonghan_attacca_op1',
+        image5: 'jeonghan_attacca_op1',
+    },
+]
