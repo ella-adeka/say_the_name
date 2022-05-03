e17caa9c4a472aeb65f3a7bb15b285ec97a4f4f0
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-05-03 23:27:56 +0100

    created function to showCarousel onclick

--- javascript/pages/members.js
+++ javascript/pages/members.js
@@ -1,16 +1,17 @@
 const main = document.querySelector('.mainMembers');
-const koreanName = document.querySelector("mainMembers__dets__info__korean_name");
 
+
+const memberCarousel = document.createElement("div");
+memberCarousel.setAttribute("class", "membersCarousel");
+
+const closeMember = document.querySelector(".close");
+const theDiv = document.querySelector(".theDiv");
 
 membersData.forEach(member => {
     const dets = document.createElement('div');
     dets.setAttribute("class", "mainMembers__dets");
     const image = 'images/members/'+member.image+".jpg";
 
-    // 
-    // var showKName = ;
-    
-    
     dets.innerHTML = `
         <div class="mainMembers__dets__imageDiv">
             <img class="mainMembers__dets__imageDiv__image" src='${image}' />
@@ -29,56 +30,106 @@ membersData.forEach(member => {
         </div>   
         <p class="mainMembers__dets__pronunciation"> ${member.pronunciation}</p>
         <p class="mainMembers__dets__number">
-            ${member.id}<sub class="totalMembers">/${membersData.length}</sub>
+            ${member.id}<span class="totalMembers">/${membersData.length}</span>
         </p>
     `;
 
-    // function doNotShow() {
-        // if (member.korean_name === undefined) {
-            // koreanName.style.display = "none";
-            // console.log("no")
-            // koreanName.setAttribute("style", "display:none");
-        // }
-    // }
     main.append(dets);
 });
 
+let i = 0;
+// eachMembersData.forEach(memberImage => {
+function showCarousel() {
+   
+    // membersData.forEach(member => {
+        let memberId = membersData.filter((member) => {member.id == i})
+        let memberPronounce = membersData.filter((member) => {member.pronunciation})
+        if (i === memberId) {
+            var pronounce = memberPronounce;
+            console.log(pronounce);
+        }
+    // })
+
+    memberCarousel.innerHTML = `
+        <ul class="membersCarousel__paragraph"> 
+            <li>
+                <span>
+                    <img class="membersCarousel__paragraph__image"  src="./images/members/${eachMembersData[i].image1}.jpg" alt="">
+                </span>
+            </li>
+            <li>
+                <span>
+                    <img class="membersCarousel__paragraph__image"  src="./images/members/${eachMembersData[i].image2}.jpg" alt="">
+                </span>
+            </li>
+            <li>
+                <span>
+                    <img class="membersCarousel__paragraph__image"  src="./images/members/${eachMembersData[i].image3}.jpg" alt="">
+                </span>
+            </li>
+            <li>
+                <span>
+                    <img class="membersCarousel__paragraph__image"  src="./images/members/${eachMembersData[i].image4}.jpg" alt="">
+                </span>
+            </li>
+            <li>
+                <span>
+                    <img class="membersCarousel__paragraph__image"  src="./images/members/${eachMembersData[i].image5}.jpg" alt="">
+                </span>
+            </li>
+        </ul>
+       
+        <p class="member_name">${pronounce}</p>
+    `;
+    theDiv.append(memberCarousel);
+}
+    
+    // console.log(memberImage.image1)
+// });
+
 // <p class="mainMembers__dets__info__korean_name" ><span class="mainMembers__dets__info__label">Korean Name:</span> ${member.korean_name}</p>
 
-const showTheDiv = document.querySelector(".mainMembers__dets__imageDiv");
 
-const closeMember = document.querySelector(".close");
-const theDiv = document.querySelector(".theDiv");
 
 let showSpecificMember = false;
 
-showTheDiv.addEventListener('click', toggleMember);
+const showTheDiv = document.querySelector(".mainMembers__dets__imageDiv__image");
 closeMember.addEventListener('click', closeMemberFunc);
+// showTheDiv.forEach(el => {
+//     el.addEventListener('click', toggleMember);
+//     // console.log("clicked");
+// });
+
+showTheDiv.addEventListener('click', toggleMember);
+
 
 function toggleMember() {
   if(!showSpecificMember) {
+    // i = eachMembersData[i];
+    // console.log(i)
+    showCarousel();
     theDiv.classList.add('show');
-    localStorage.setItem('member', 'showing');
-    console.log("showing");
+    // localStorage.setItem('member', 'showing');
+    // console.log("showing");
 
     showSpecificMember = true;
   } else {
     theDiv.classList.remove('show');
-    localStorage.setItem('member', 'not showing');
-    console.log("not showing");
+    // localStorage.setItem('member', 'not showing');
+    // console.log("not showing");
 
     showSpecificMember = false;
   }
 }
 
 function closeMemberFunc() {
-    localStorage.setItem('member', 'not showing');
+    // localStorage.setItem('member', 'not showing');
     theDiv.classList.remove('show');
 
     showSpecificMember = false;
-    console.log("false")
+    // console.log("false")
 }
 
-if (localStorage.getItem("member") === "showing") {
-    theDiv.classList.add('show');
-}
+// if (localStorage.getItem("member") === "showing") {
+//     theDiv.classList.add('show');
+// }
