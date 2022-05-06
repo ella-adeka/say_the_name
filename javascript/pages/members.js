b7b7dd0f5af70f19506058ca7d0008d1a9a2b779
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-05-06 22:49:49 +0100

    created members() and changeOnHover() for members

--- javascript/pages/members.js
+++ javascript/pages/members.js
@@ -7,74 +7,109 @@ memberCarousel.setAttribute("class", "membersCarousel");
 const closeMember = document.querySelector(".close");
 const theDiv = document.querySelector(".theDiv");
 
-membersData.forEach(member => {
-    const dets = document.createElement('div');
-    dets.setAttribute("class", "mainMembers__dets");
-    const image = 'images/members/'+member.image+".jpg";
 
-    dets.innerHTML = `
+
+const dets = document.createElement('div');
+dets.setAttribute("class", "mainMembers__dets");
+
+let j = 0;
+function members() {
+    if (j<membersData.length) {
+        
+        dets.innerHTML = `
         <div class="mainMembers__dets__imageDiv">
-            <img class="mainMembers__dets__imageDiv__image" src='${image}' />
+            <img class="mainMembers__dets__imageDiv__image" src='images/members/${membersData[j].image}' />
         </div>
         
         <div class="mainMembers__dets__info">
-            <p class="mainMembers__dets__info__name">${member.stage_name}<span>${member.korean_stage_name}</span></p>
-            <p class="mainMembers__dets__info__subunit">${member.sub_unit}</p>
+            <p class="mainMembers__dets__info__name">${membersData[j].stage_name}<span>${membersData[j].korean_stage_name}</span></p>
+            <p class="mainMembers__dets__info__subunit">${membersData[j].sub_unit}</p>
             
-            <p class="mainMembers__dets__info__birth_name"><span class="mainMembers__dets__info__label">Name:</span> ${member.birth_name}</p>
+            <p class="mainMembers__dets__info__birth_name"><span class="mainMembers__dets__info__label">Name:</span> ${membersData[j].birth_name}</p>
             
-            <p class="mainMembers__dets__info__nationality"><span class="mainMembers__dets__info__label">Nationality:</span> ${member.nationality}</p>
-            <p class="mainMembers__dets__info__position"><span class="mainMembers__dets__info__label">Position:</span> ${member.position}</p>
-            <p class="mainMembers__dets__info__birthday"><span class="mainMembers__dets__info__label">Birthday:</span> ${member.birthday}</p>
-            <p class="mainMembers__dets__info__zodiac"><span class="mainMembers__dets__info__label">Zodiac Sign:</span> ${member.zodiac_sign}</p>
+            <p class="mainMembers__dets__info__nationality"><span class="mainMembers__dets__info__label">Nationality:</span> ${membersData[j].nationality}</p>
+            <p class="mainMembers__dets__info__position"><span class="mainMembers__dets__info__label">Position:</span> ${membersData[j].position}</p>
+            <p class="mainMembers__dets__info__birthday"><span class="mainMembers__dets__info__label">Birthday:</span> ${membersData[j].birthday}</p>
+            <p class="mainMembers__dets__info__zodiac"><span class="mainMembers__dets__info__label">Zodiac Sign:</span> ${membersData[j].zodiac_sign}</p>
         </div>   
-        <p class="mainMembers__dets__pronunciation"> ${member.pronunciation}</p>
+        <p class="mainMembers__dets__pronunciation"> ${membersData[j].pronunciation}</p>
         <p class="mainMembers__dets__number">
-            ${member.id}<span class="totalMembers">/${membersData.length}</span>
+            ${membersData[j].id}<span class="totalMembers">/${membersData.length}</span>
         </p>
-    `;
+        `;
+        main.append(dets);
+    }
 
-    main.append(dets);
-});
 
-let i = 0;
-// eachMembersData.forEach(memberImage => {
-function showCarousel() {
-   
-    // membersData.forEach(member => {
-        let memberId = membersData.filter((member) => {member.id == i})
-        let memberPronounce = membersData.filter((member) => {member.pronunciation})
-        if (i === memberId) {
-            var pronounce = memberPronounce;
-            console.log(pronounce);
-        }
-    // })
+    let showSpecificMember = false;
+
+    const showTheDiv = document.querySelector(".mainMembers__dets__imageDiv__image");
+    closeMember.addEventListener('click', closeMemberFunc);
+
+    showTheDiv.addEventListener('click', toggleMember);
+
+
+    function toggleMember() {
+    if(!showSpecificMember) {
+        // i = eachMembersData[i];
+        // console.log(i)
+        showCarousel(j);
+        theDiv.classList.add('show');
+        // localStorage.setItem('member', 'showing');
+        // console.log("showing");
+
+        showSpecificMember = true;
+    } else {
+        theDiv.classList.remove('show');
+        // localStorage.setItem('member', 'not showing');
+        // console.log("not showing");
+
+        showSpecificMember = false;
+    }
+    }
+
+    function closeMemberFunc() {
+        // localStorage.setItem('member', 'not showing');
+        theDiv.classList.remove('show');
+
+        showSpecificMember = false;
+        // console.log("false")
+    }
+    showCarousel(j);
+    changeOnHover(j)
+}
+
+
+
+
+function showCarousel(j) {
+    var pronounce = membersData[j].pronunciation;
 
     memberCarousel.innerHTML = `
         <ul class="membersCarousel__paragraph"> 
             <li>
                 <span>
-                    <img class="membersCarousel__paragraph__image"  src="./images/members/${eachMembersData[i].image1}.jpg" alt="">
+                    <img class="membersCarousel__paragraph__image"  src="${membersData[j].imagesGrp[0]}" alt="">
                 </span>
             </li>
             <li>
                 <span>
-                    <img class="membersCarousel__paragraph__image"  src="./images/members/${eachMembersData[i].image2}.jpg" alt="">
+                    <img class="membersCarousel__paragraph__image"  src="${membersData[j].imagesGrp[1]}" alt="">
                 </span>
             </li>
             <li>
                 <span>
-                    <img class="membersCarousel__paragraph__image"  src="./images/members/${eachMembersData[i].image3}.jpg" alt="">
+                    <img class="membersCarousel__paragraph__image"  src="${membersData[j].imagesGrp[2]}" alt="">
                 </span>
             </li>
             <li>
                 <span>
-                    <img class="membersCarousel__paragraph__image"  src="./images/members/${eachMembersData[i].image4}.jpg" alt="">
+                    <img class="membersCarousel__paragraph__image"  src="${membersData[j].imagesGrp[3]}" alt="">
                 </span>
             </li>
             <li>
                 <span>
-                    <img class="membersCarousel__paragraph__image"  src="./images/members/${eachMembersData[i].image5}.jpg" alt="">
+                    <img class="membersCarousel__paragraph__image"  src="${membersData[j].imagesGrp[4]}" alt="">
                 </span>
             </li>
         </ul>
@@ -83,53 +118,31 @@ function showCarousel() {
     `;
     theDiv.append(memberCarousel);
 }
-    
-    // console.log(memberImage.image1)
-// });
-
-// <p class="mainMembers__dets__info__korean_name" ><span class="mainMembers__dets__info__label">Korean Name:</span> ${member.korean_name}</p>
 
 
+function changeOnHover(j) {
+    // const imageDiv = document.querySelectorAll(".mainMembers__dets__imageDiv");
+    // const imageInDiv = document.querySelectorAll(".mainMembers__dets__imageDiv__image");
 
-let showSpecificMember = false;
-
-const showTheDiv = document.querySelector(".mainMembers__dets__imageDiv__image");
-closeMember.addEventListener('click', closeMemberFunc);
-// showTheDiv.forEach(el => {
-//     el.addEventListener('click', toggleMember);
-//     // console.log("clicked");
-// });
-
-showTheDiv.addEventListener('click', toggleMember);
+    // var firstDiv = imageDiv[0];
+    // var firstInDiv = imageInDiv[0];
 
 
-function toggleMember() {
-  if(!showSpecificMember) {
-    // i = eachMembersData[i];
-    // console.log(i)
-    showCarousel();
-    theDiv.classList.add('show');
-    // localStorage.setItem('member', 'showing');
-    // console.log("showing");
+    // firstDiv.addEventListener("mouseover", () => {
+    //     firstInDiv.setAttribute("src", 'images/attacca_op3/'+membersData[j].hoverImage);
+    // });
 
-    showSpecificMember = true;
-  } else {
-    theDiv.classList.remove('show');
-    // localStorage.setItem('member', 'not showing');
-    // console.log("not showing");
+    // firstDiv.addEventListener("mouseout", () => {
+    //     firstInDiv.setAttribute("src", 'images/members/'+membersData[j].image);
+    // });
+   
+    new hoverEffect({
+        parent: document.querySelector('.mainMembers__dets__imageDiv__image'),
+        intensity: 0.5,
+        image1: 'images/attacca_op3/'+membersData[j].hoverImage,
+        image2: 'images/members/'+membersData[j].image,
+        displacementImage: 'images/seventeen_logo.png'
+    });
 
-    showSpecificMember = false;
-  }
+    console.log("changed")
 }
-
-function closeMemberFunc() {
-    // localStorage.setItem('member', 'not showing');
-    theDiv.classList.remove('show');
-
-    showSpecificMember = false;
-    // console.log("false")
-}
-
-// if (localStorage.getItem("member") === "showing") {
-//     theDiv.classList.add('show');
-// }
