5a3a70ab01ff80903d71c989b51cca2ffa99a8ce
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-05-08 23:18:03 +0100

    added condition to shpw korean_name if there is one

--- javascript/pages/members.js
+++ javascript/pages/members.js
@@ -12,9 +12,17 @@ const theDiv = document.querySelector(".theDiv");
 const dets = document.createElement('div');
 dets.setAttribute("class", "mainMembers__dets");
 
-let j = 0;
+let j = 2;
 function members() {
     if (j<membersData.length) {
+
+        let korean_name_var = membersData[j].korean_name;
+        let korean_name;
+        if (typeof korean_name_var != 'undefined') {
+            korean_name = `<p class="mainMembers__dets__info__korean_name"><span class="mainMembers__dets__info__label">Korean Name:</span> ${membersData[j].korean_name}</p>`;
+        } else{
+            korean_name = `<span><span>`;
+        }
         
         dets.innerHTML = `
         <div class="mainMembers__dets__imageDiv">
@@ -25,10 +33,10 @@ function members() {
             <p class="mainMembers__dets__info__name">${membersData[j].stage_name}<span>${membersData[j].korean_stage_name}</span></p>
             <p class="mainMembers__dets__info__subunit">${membersData[j].sub_unit}</p>
             
-            <p class="mainMembers__dets__info__birth_name"><span class="mainMembers__dets__info__label">Name:</span> ${membersData[j].birth_name}</p>
-            
-            <p class="mainMembers__dets__info__nationality"><span class="mainMembers__dets__info__label">Nationality:</span> ${membersData[j].nationality}</p>
+            <p class="mainMembers__dets__info__birth_name"><span class="mainMembers__dets__info__label">Birth Name:</span> ${membersData[j].birth_name}</p>
+            ${korean_name}
             <p class="mainMembers__dets__info__position"><span class="mainMembers__dets__info__label">Position:</span> ${membersData[j].position}</p>
+            <p class="mainMembers__dets__info__nationality"><span class="mainMembers__dets__info__label">Nationality:</span> ${membersData[j].nationality}</p>
             <p class="mainMembers__dets__info__birthday"><span class="mainMembers__dets__info__label">Birthday:</span> ${membersData[j].birthday}</p>
             <p class="mainMembers__dets__info__zodiac"><span class="mainMembers__dets__info__label">Zodiac Sign:</span> ${membersData[j].zodiac_sign}</p>
         </div>   
@@ -121,28 +129,28 @@ function showCarousel(j) {
 
 
 function changeOnHover(j) {
-    // const imageDiv = document.querySelectorAll(".mainMembers__dets__imageDiv");
-    // const imageInDiv = document.querySelectorAll(".mainMembers__dets__imageDiv__image");
+    const imageDiv = document.querySelectorAll(".mainMembers__dets__imageDiv");
+    const imageInDiv = document.querySelectorAll(".mainMembers__dets__imageDiv__image");
 
-    // var firstDiv = imageDiv[0];
-    // var firstInDiv = imageInDiv[0];
+    var firstDiv = imageDiv[0];
+    var firstInDiv = imageInDiv[0];
 
 
-    // firstDiv.addEventListener("mouseover", () => {
-    //     firstInDiv.setAttribute("src", 'images/attacca_op3/'+membersData[j].hoverImage);
-    // });
-
-    // firstDiv.addEventListener("mouseout", () => {
-    //     firstInDiv.setAttribute("src", 'images/members/'+membersData[j].image);
-    // });
-   
-    new hoverEffect({
-        parent: document.querySelector('.mainMembers__dets__imageDiv__image'),
-        intensity: 0.5,
-        image1: 'images/attacca_op3/'+membersData[j].hoverImage,
-        image2: 'images/members/'+membersData[j].image,
-        displacementImage: 'images/seventeen_logo.png'
+    firstDiv.addEventListener("mouseover", () => {
+        firstInDiv.setAttribute("src", 'images/attacca_op3/'+membersData[j].hoverImage);
     });
+
+    firstDiv.addEventListener("mouseout", () => {
+        firstInDiv.setAttribute("src", 'images/members/'+membersData[j].image);
+    });
+   
+    // new hoverEffect({
+    //     parent: document.querySelector('.mainMembers__dets__imageDiv__image'),
+    //     intensity: 0.9,
+    //     image1: 'images/members/'+membersData[j].image,
+    //     image2: 'images/attacca_op3/'+membersData[j].hoverImage,
+    //     displacementImage: 'images/heightMap.png'
+    // });
 
     console.log("changed")
 }
