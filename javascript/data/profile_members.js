1f31e17860fa830c23191a5fd7b2f8d02c0aa7db
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-05-21 22:43:20 +0100

    added path to member image

--- javascript/data/profile_members.js
+++ javascript/data/profile_members.js
@@ -4,79 +4,79 @@ const profileMembersData = [
     {
         id: "01",
         name: "S.Coups",
-        image: "scoups_attacca_op1",
+        image: "scoups/scoups_attacca_op1",
         unit: "Hip-Hop Team"
     },
     {
         id: "02",
         name: "Wonwoo",
-        image: "wonwoo_attacca_op1",
+        image: "wonwoo/wonwoo_attacca_op1",
         unit: "Hip-Hop Team"
     },
     {
         id: "03",
         name: "Mingyu",
-        image: "mingyu_attacca_op1",
+        image: "mingyu/mingyu_attacca_op1",
         unit: "Hip-Hop Team"
     },
     {
         id: "04",
         name: "Vernon",
-        image: "vernon_attacca_op1",
+        image: "vernon/vernon_attacca_op1",
         unit: "Hip-Hop Team"
     },
     {
         id: "05",
         name: "Woozi",
-        image: "woozi_attacca_op1",
+        image: "woozi/woozi_attacca_op1",
         unit: "Vocal Team"
     },
     {
         id: "06",
         name: "Jeonghan",
-        image: "jeonghan_attacca_op1",
+        image: "jeonghan/jeonghan_attacca_op1",
         unit: "Vocal Team"
     },
     {
         id: "07",
         name: "Joshua",
-        image: "joshua_attacca_op1",
+        image: "joshua/joshua_attacca_op1",
         unit: "Vocal Team"
     },
     {
         id: "08",
         name: "DK",
-        image: "dk_attacca_op1",
+        image: "dk/dk_attacca_op1",
         unit: "Vocal Team"
     },
     {
         id: "09",
         name: "Seungkwan",
-        image: "seungkwan_attacca_op1",
+        image: "seungkwan/seungkwan_attacca_op1",
         unit: "Vocal Team"
     },
     {
         id: "10",
         name: "Hoshi",
-        image: "hoshi_attacca_op1",
+        image: "hoshi/hoshi_attacca_op1",
         unit: "Performance Team"
     },
     {
         id: "11",
         name: "Jun",
-        image: "jun_attacca_op1",
+        image: "jun/jun_attacca_op1",
         unit: "Performance Team"
     },
     {
         id: "12",
         name: "Minghao",
-        image: "minghao_attacca_op1",
+        image: "minghao/minghao_attacca_op1",
         unit: "Performance Team"
     },
     {
         id: "13",
         name: "Dino",
-        image: "dino_attacca_op1",
+        image: "dino/dino_attacca_op1",
         unit: "Performance Team"
     },
 ]
